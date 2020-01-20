ecefa21ed5df0a3d09039608f29b3bc5485994bd
Mburiah <bykieswagix5@gmail.com>
2020-01-20 12:24:51 +0300

    commented out db credentials for local

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,6 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild_track", "postgres", "password");
-//        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-33-75.compute-1.amazonaws.com:5432/dfki83t6hkdabm", "lxzyxrnzqubzas", "54acab5be4c3552497e0de92d734ee4c789d1cd828c648e8a51470908de26647");
+//        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wild_track", "postgres", "password");
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-33-75.compute-1.amazonaws.com:5432/dfki83t6hkdabm", "lxzyxrnzqubzas", "54acab5be4c3552497e0de92d734ee4c789d1cd828c648e8a51470908de26647");
 }
